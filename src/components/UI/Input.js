8f961174335bdff557ba7d48c240add9b3cdcc0f
Mathieu Favre <matfav@hotmail.fr>
2023-02-01 13:44:03 +0100

    Added Cart logic, to handle addition of meal items, and display in the cart modal

--- src/components/UI/Input.js
+++ src/components/UI/Input.js
@@ -1,12 +1,12 @@
-
+import React from 'react'
 import styles from './Input.module.css'
 
 
-const Input = props => {
+const Input = React.forwardRef((props, ref) => {
     return <div className={styles.input}>
         <label htmlFor={props.input.id}>{props.label}</label>
-        <input {...props.input} />                   {/* Spreading props allows the addition of attributes automatically with React */}
+        <input ref={ref} {...props.input} />                   {/* Spreading props allows the addition of attributes automatically with React */}
     </div>
-}
+})
 
 export default Input
--- src/store/CartProvider.js
+++ src/store/CartProvider.js
@@ -1,4 +1,4 @@
-import { useDeferredValue, useReducer } from 'react'
+import { useReducer } from 'react'
 
 import CartContext from "./cart-context";
 
@@ -9,8 +9,11 @@ const defaultCartState = {
 
 const cartReducer = (state, action) => {
     if (action.type === 'ADD') {
-        const updatedItems = state.items.concat(action.item);
         const updatedTotalAmount = state.totalAmount + action.item.price * action.item.amount;
+        const itemAlreadyInCart = state.items.findIndex(
+            item => item.id === action.item.id
+        )
+        const updatedItems = state.items.concat(action.item);
         return {
             items: updatedItems,
             totalAmount: updatedTotalAmount
