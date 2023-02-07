a6bd74dfb749c6b5067ddda1749c291434d4bcd9
Mathieu Favre <matfav@hotmail.fr>
2023-02-07 10:00:32 +0100

    Corrected 'Add' button issue, items are now added correctly to cart

--- src/components/Meals/MealItem/MealItemForm.js
+++ src/components/Meals/MealItem/MealItemForm.js
@@ -21,7 +21,7 @@ const MealItemForm = props => {
     return (
         <form className={styles.form} onSubmit={submitHandler}>
             <Input ref={amountInputRef} label='Amount' input={{ id: 'amount_' + props.id, type: 'number', min: '1', max: '5', step: '1', defaultValue: '1' }} />
-            <button type='button'>Add</button>
+            <button>Add</button>
             {!amountIsValid && <p>Please enter a valid amount (1-5)</p>}
         </form>)
 }
--- src/store/CartProvider.js
+++ src/store/CartProvider.js
@@ -9,20 +9,15 @@ const defaultCartState = {
 
 const cartReducer = (state, action) => {
     if (action.type === 'ADD') {
-        const updatedTotalAmount = state.totalAmount + action.item.price * action.item.amount;
-        const itemAlreadyInCart = state.items.findIndex(
-            item => item.id === action.item.id
-        )
         const updatedItems = state.items.concat(action.item);
+        const updatedTotalAmount = state.totalAmount + action.item.price * action.item.amount;
         return {
             items: updatedItems,
             totalAmount: updatedTotalAmount
-        }
-    } if (action.type === 'REMOVE') {
-
+        };
     }
     return defaultCartState;
-}
+};
 
 const CartProvider = props => {
     const [cartState, dispatchCartAction] = useReducer(cartReducer, defaultCartState);
@@ -38,8 +33,8 @@ const CartProvider = props => {
     const cartContext = {
         items: cartState.items,
         totalAmount: cartState.totalAmount,
-        addItem: (item) => { },
-        removeItem: (id) => { }
+        addItem: addItemToCartHandler,
+        removeItem: removeItemFromCartHandler
     };
 
     return <CartContext.Provider value={cartContext}>
